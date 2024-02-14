d07ea6b200456d9f55b0266ff78b0616d3f0d206
jonscales <jon.scales@msutexas.edu>
2024-02-14 15:19:47 -0600

    bubble started

--- Project2_5243.py
+++ Project2_5243.py
@@ -9,18 +9,21 @@ import random
 """ List initializations
     generate a random array based on options
 """
-def ListInit(size, start_range, end_range):
-    # start_range = int(input("Enter the start range for a randomly generated set of array values: "))
-    # end_range=int(input("Enter the end range for a randomly generated set of array values: "))
-    # size = int(input("Enter the number of elements in the array to be randomly generated: "))
-   
-    
+def listInit(size, start_range, end_range): 
     print(f"Random range begins at: ",start_range, " and ends at: ", end_range)
     print(f'The size of the list is ',size, ' elements')
     # generate random values to initialize array
     unsorted_list=[random.randint(start_range, end_range) for _ in range(size)]
     return unsorted_list
 
+def randomize(sorted_list):
+    unsorted_list = random.shuffle(sorted_list)
+    return unsorted_list
+
+def swap(x,y):
+    return y, x
+    
+    
 
 list_options ={ 
                 "0":["TestList",[0,10,100]],
@@ -41,26 +44,38 @@ sort_options={
 """
 Bubble Sort
 """
-def BubbleSort(list):
-    pass
+def bubbleSort(list):
+    swapped = True
+    n=len(list)
+    i=0
+    while swapped: 
+        swapped = False
+        for i in range(n-1):
+            if unsorted_list[i]>unsorted_list[i+1]:
+                swap(unsorted_list[i],unsorted_list[i+1])
+                swapped = True
+                print("made a swap")
+        n-=1    
+                     
+    return list    
+    
 
 """
 Merge Sort
 """
-def MergeSort(list):
-    pass
+def mergeSort(list):
+    return list
 
 """
 Selection Sort
 """
-def SelectionSort(list):
-    pass
-
+def selectionSort(list):
+    return list
 """
 Insertion Sort
 """
-def InsertionSort(list):
-    pass
+def insertionSort(list):
+    return list
 
 if __name__== "__main__":
 
@@ -81,37 +96,36 @@ if __name__== "__main__":
         
         # call the list initialization function
         
-        unsorted_list = ListInit(size, start_range, end_range)
+        unsorted_list = listInit(size, start_range, end_range)
         print("The unsorted list is: ", unsorted_list)
-        # # Display sort options to the user
-        # print("Choose an option for the type of sort to run:")
-        # for option, values in sort_options.items():
-        #     list_type,_ = values
-        #     print(f"{option}: {list_type}")
+        
+        # Display sort options to the user
+        print("Choose an option for the type of sort to run:")
+        for option, values in sort_options.items():
+            list_type,_ = values
+            print(f"{option}: {list_type}")
     
-        # # Prompt user for sort type input
-        # sort_choice = input("Enter the number for the type of sort you want to perform: ")
+        # Prompt user for sort type input
+        sort_choice = input("Enter the number for the type of sort you want to perform: ")
     
-        # # Check if the user input corresponds to a valid option
-        # if list_choice in list_options:
-        #     # Get the corresponding list
-        #     selected_list = list_options[list_choice]
-        #     # Call the sort function with the selected list
-        #     if list_choice == 1:
-        #         BubbleSort(selected_list)
+        # Check if the user input corresponds to a valid option
+        if list_choice in list_options:
+            # Get the corresponding list
+            selected_list = list_options[list_choice]
+            # Call the sort function with the selected list
+            if list_choice == 1:
+                bubbleSort(unsorted_list)
 
-        #     elif list_choice == 2:
-        #         SelectionSort(selected_list)
+            elif list_choice == 2:
+                selectionSort(unsorted_list)
 
-        #     elif list_choice == 3:
-        #         InsertionSort(selected_list)
+            elif list_choice == 3:
+                insertionSort(unsorted_list)
 
-        #     elif list_choice == 4:
-        #         MergeSort(selected_list)
-            
-
-        # else:
-        #     print("Invalid option. Please choose from the given options.")
+            else:
+                mergeSort(unsorted_list)
+        else:
+            print("Invalid option. Please choose from the given options.")
     else:
         print("Invalid option. Please choose from the given options.")
    
