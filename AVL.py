c738f86d0a305ac991514c1649a1f4f39adbcb9d
jonscales <jon.scales@msutexas.edu>
2024-04-10 10:14:36 -0500

    avl_word version

--- AVL.py
+++ AVL.py
@@ -4,19 +4,21 @@ Code copied from bfaure/Python3_Data_Structures GitHub
 https://github.com/bfaure/Python3_Data_Structures/blob/master/AVL_Tree/main.py
 """
 class node:
-	def __init__(self,value=None):
-		self.value=value
+	def __init__(self,word=None):
+		self.word=word
 		self.left_child=None
 		self.right_child=None
 		self.parent=None # pointer to parent node in tree
 		self.height=1 # height of node in tree (max dist. to leaf) NEW FOR AVL
-		self.balance_factor=0
+		self.balance_factor=0 # store a balance factor calculated from the height calculations
 
 class AVLTree:
 	def __init__(self):
+		"""constructor method"""
 		self.root=None
 
 	def __repr__(self):
+		"""function to display the tree sort of """
 		if self.root==None: return ''
 		content='\n' # to hold final string
 		cur_nodes=[self.root] # all nodes at current level
@@ -40,9 +42,9 @@ class AVLTree:
 					next_nodes.extend([None,None])
 					continue
 
-				if n.value!=None:       
-					buf=' '*int((5-len(str(n.value)))/2)
-					cur_row+='%s%s%s'%(buf,str(n.value),buf)+sep
+				if n.word!=None:       
+					buf=' '*int((5-len(str(n.word)))/2)
+					cur_row+='%s%s%s'%(buf,str(n.word),buf)+sep
 				else:
 					cur_row+=' '*5+sep
 
@@ -65,31 +67,32 @@ class AVLTree:
 			sep=' '*int(len(sep)/2) # cut separator size in half
 		return content
 
-	def insert(self,value):
-		"""Called to insert a value into the tree"""
+	def insert(self,word):
+		"""Called to insert a word into the tree"""
 		if self.root==None:
-			self.root=node(value)
+			self.root=node(word)
 		else:
-			self._insert(value,self.root)
+			self._insert(word,self.root)
 
-	def _insert(self,value,cur_node):
-		"""Called recursively by the base insert function to put value into correct location"""
-		if value<cur_node.value:
+	def _insert(self,word,cur_node):
+		"""Called recursively by the base insert function to put word into correct location
+			will not insert duplicate words"""
+		if word<cur_node.word:
 			if cur_node.left_child==None:
-				cur_node.left_child=node(value)
+				cur_node.left_child=node(word)
 				cur_node.left_child.parent=cur_node # set parent
 				self._inspect_insertion(cur_node.left_child)
 			else:
-				self._insert(value,cur_node.left_child)
-		elif value>cur_node.value:
+				self._insert(word,cur_node.left_child)
+		elif word>cur_node.word:
 			if cur_node.right_child==None:
-				cur_node.right_child=node(value)
+				cur_node.right_child=node(word)
 				cur_node.right_child.parent=cur_node # set parent
 				self._inspect_insertion(cur_node.right_child)
 			else:
-				self._insert(value,cur_node.right_child)
+				self._insert(word,cur_node.right_child)
 		else:
-			print("Value already in tree!")
+			print("word already in tree!")
 
 	def print_tree(self):
 		if self.root!=None:
@@ -98,51 +101,54 @@ class AVLTree:
 	def _print_tree(self,cur_node):
 		if cur_node!=None:
 			self._print_tree(cur_node.left_child)
-			print ('%s, h=%d'%(str(cur_node.value),cur_node.height))
+			print ('%s, h=%d'%(str(cur_node.word),cur_node.height))
 			self._print_tree(cur_node.right_child)
 
 	def height(self):
+		""" called to see if a tree exits, if so recursively calls _height()"""
 		if self.root!=None:
 			return self._height(self.root,0)
 		else:
 			return 0
 
 	def _height(self,cur_node,cur_height):
+		"""recursive height function continually passes in child nodes until reaching leaf
+		   and adds height to node's height parameter. returns greates of left or right path"""
 		if cur_node==None: return cur_height
 		left_height=self._height(cur_node.left_child,cur_height+1)
 		right_height=self._height(cur_node.right_child,cur_height+1)
 		return max(left_height,right_height)
 
-	def find(self,value):
-		"""Called to find a value in the tree"""
+	def find(self,word):
+		"""Called to find a word in the tree"""
 		if self.root!=None:
-			return self._find(value,self.root)
+			return self._find(word,self.root)
 		else:
 			return None
 
-	def _find(self,value,cur_node):
-		"""Called recursively by the base find function to find the value"""
-		if value==cur_node.value:
+	def _find(self,word,cur_node):
+		"""Called recursively by the base find function to find the word"""
+		if word==cur_node.word:
 			return cur_node
-		elif value<cur_node.value and cur_node.left_child!=None:
-			return self._find(value,cur_node.left_child)
-		elif value>cur_node.value and cur_node.right_child!=None:
-			return self._find(value,cur_node.right_child)
+		elif word<cur_node.word and cur_node.left_child!=None:
+			return self._find(word,cur_node.left_child)
+		elif word>cur_node.word and cur_node.right_child!=None:
+			return self._find(word,cur_node.right_child)
 
-	def delete_value(self,value):
-		"""Called to delete a value from the tree"""
-		return self.delete_node(self.find(value))
+	def delete_word(self,word):
+		"""Called to delete a word from the tree"""
+		return self.delete_node(self.find(word))
 
 	def delete_node(self,node):
-		"""Called recursively by the delete_value function to delete a node with the value
+		"""Called recursively by the delete_word function to delete a node with the word
 		   uses multiple helper functions to get features of the nodes"""
 		# Protect against deleting a node not found in the tree
-		if node==None or self.find(node.value)==None:
+		if node==None or self.find(node.word)==None:
 			print("Node to be deleted not found in the tree!")
 			return None 
 		
-		# returns the node with min value in tree rooted at input node
-		def min_value_node(n):
+		# returns the node with min word in tree rooted at input node
+		def min_word_node(n):
 			current=n
 			while current.left_child!=None:
 				current=current.left_child
@@ -201,13 +207,13 @@ class AVLTree:
 		if node_children==2:
 
 			# get the inorder successor of the deleted node
-			successor=min_value_node(node.right_child)
+			successor=min_word_node(node.right_child)
 
-			# copy the inorder successor's value to the node formerly
-			# holding the value we wished to delete
-			node.value=successor.value
+			# copy the inorder successor's word to the node formerly
+			# holding the word we wished to delete
+			node.word=successor.word
 
-			# delete the inorder successor now that it's value was
+			# delete the inorder successor now that it's word was
 			# copied into the other node
 			self.delete_node(successor)
 
@@ -222,19 +228,19 @@ class AVLTree:
 			# any nodes which now invalidate the AVL balance rules
 			self._inspect_deletion(node_parent)
 
-	def search(self,value):
+	def search(self,word):
 		if self.root!=None:
-			return self._search(value,self.root)
+			return self._search(word,self.root)
 		else:
 			return False
 
-	def _search(self,value,cur_node):
-		if value==cur_node.value:
+	def _search(self,word,cur_node):
+		if word==cur_node.word:
 			return True
-		elif value<cur_node.value and cur_node.left_child!=None:
-			return self._search(value,cur_node.left_child)
-		elif value>cur_node.value and cur_node.right_child!=None:
-			return self._search(value,cur_node.right_child)
+		elif word<cur_node.word and cur_node.left_child!=None:
+			return self._search(word,cur_node.left_child)
+		elif word>cur_node.word and cur_node.right_child!=None:
+			return self._search(word,cur_node.right_child)
 		return False 
 
 	# Functions for AVL balancing
@@ -337,24 +343,15 @@ class AVLTree:
 	
 
 avl=AVLTree()
-avl.insert(50)
-avl.insert(75)
-avl.insert(85)
-avl.insert(10)
-avl.insert(8)
-avl.insert(3)
-avl.insert(60)
-avl.insert(55)
+avl.insert('apple')
+avl.insert('box')
+avl.insert('revolver')
+avl.insert('revolve')
+avl.insert('document')
+avl.insert('pencil')
+avl.insert('zebra')
+avl.insert('lion')
 
-#avl.print_tree()
-print(avl)
-
-avl.delete_value(50)
+avl.print_tree()
 print(avl)
 
-avl.delete_value(75)
-print(avl)
-avl.delete_value(3)
-print(avl)
-avl.delete_value(8)
-print(avl)
